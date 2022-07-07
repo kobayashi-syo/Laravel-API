6bd966cf0c9e0f01e482082841df25f0005c4a83
Syo-Kobayashi <kobayashi@crunchtimer.jp>
2022-07-07 09:49:19 +0900

    swaggerインストール

---
+++ src/app/Http/Controllers/TestController.php
@@ -0,0 +1,46 @@
+    /**
+    * @OA\Get(
+    * path="/test",
+    * summary="Swaggerのテスト用",
+    * @OA\Response(
+    * response=200,
+    * description="OK",
+    * @OA\JsonContent(
+    * type="object",
+    * @OA\Property(
+    * property="message",
+    * type="string",
+    * description="レスポンスパラメータの例を記載"
+    * )
+    * )
+    * ),
+    * @OA\Response(
+    * response=400,
+    * description="Bad Request",
+    * @OA\JsonContent(
+    * type="object",
+    * @OA\Property(
+    * property="message",
+    * type="string",
+    * description="レスポンスパラメータの例を記載"
+    * )
+    * )
+    * ),
+    * @OA\Response(
+    * response="default",
+    * description="Unexpected Error",
+    * @OA\JsonContent(
+    * type="object",
+    * @OA\Property(
+    * property="message",
+    * type="string",
+    * description="レスポンスパラメータの例を記載"
+    * )
+    * )
+    * )
+    * )
+    */
+    public function index()
+    {
+    //
+    }
